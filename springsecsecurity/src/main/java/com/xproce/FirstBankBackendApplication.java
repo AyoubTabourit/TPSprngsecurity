3c365f48b38f0de9889dce0898bcaf295e5196f8
ayoub <ayoub05@gmail.com>
2024-01-15 21:08:19 +0100

    first

---
+++ springsecsecurity/src/main/java/com/xproce/FirstBankBackendApplication.java
@@ -0,0 +1,13 @@
+package com.xproce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstBankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstBankBackendApplication.class, args);
+	}
+
+}
---
+++ springsecsecurity/src/main/java/com/xproce/entities/bankBackendApplication.java
@@ -0,0 +1,13 @@
+package com.xproce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class bankBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(bankBackendApplication.class, args);
+    }
+
+}
